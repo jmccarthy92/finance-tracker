e95b831c270fcecd62799ce34856a6aec26bad2e
jmccarthy92 <jmccarthyns92@gmail.com>
2018-07-18 20:40:27 +0000

    Finished finance-tracker app

---
+++ app/assets/javascripts/friends.js
@@ -0,0 +1,5 @@
+// $(document).on('turbolinks:load',function(){
+//   $('#friends-lookup-form').on('ajax:complete', function(event, data, status){
+//     $('#results').html(data.responseText);
+//   })
+// })
